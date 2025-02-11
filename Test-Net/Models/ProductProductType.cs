0afca2f05f666188a204d552d1f14361f7ca9919
TrinhDucDuong <128205570+MEEP-D@users.noreply.github.com>
2025-02-11 15:31:17 +0700

    Update ProductProductType.cs

--- Test-Net/Models/ProductProductType.cs
+++ Test-Net/Models/ProductProductType.cs
@@ -5,7 +5,7 @@ namespace Test_Net.Models
     public class ProductProductType
     {
         [Key]
-        public int Id { get; set; } // Khóa chính (Primary Key)
+        public int Id { get; set; } 
         public int ProductId { get; set; }
         public Product Product { get; set; }
 
